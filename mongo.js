8ead5c20391f787374f046bbaa9f1cd6464cdd07
AratioD <fffb@ddd.com>
2020-01-26 21:51:07 +0200

    school example works better

--- mongo.js
+++ mongo.js
@@ -15,26 +15,88 @@
 //     const db = client.db(dbName);
 
 //     client.close();
+// // });
+
+// var process = require("process")
+// const mongoose = require("mongoose");
+
+// mongoose.connect("mongodb://localhost/customer", { useNewUrlParser: true })
+
+// if (process.pid) {
+//     console.log('This process is your pid ' + process.pid);
+// }
+
+// mongoose.connection.once("open", () => console.log("Connected"))
+// mongoose.connection.on("error", (error) => {
+//     console.log("my error", error)
+// })
+// mongoose.connection.collection.
+
+// mongoose.collection('customer').find().toArray(function (err, docs) {
+//     console.log(JSON.stringify(docs));
 // });
+//const mongodb = require('mongodb')
+const mongoClient = require('mongodb').MongoClient
 
-var process = require("process")
-const mongoose = require("mongoose");
+const url = 'mongodb://localhost:27017/customer'
 
-mongoose.connect("mongodb://localhost/customer", { useNewUrlParser: true })
+// create new devices collection and add some data into it
+mongoClient.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },
+  (err, db) => {
+    if (err) {
+      console.log('ERROR:', err)
+    } 
+    else {
+      console.log('CONNECTED: ', url)
 
-if (process.pid) {
-    console.log('This process is your pid ' + process.pid);
-}
+    //   const demoDb = db.db('nodedemos')
 
-mongoose.connection
-    .once("open", () => console.log("Connected"))
-    .on("error", (error) => {
-        console.log("my error", error)
-    })
+    //   const collection = demoDb.collection('devices')
 
+    //   const devArr = []
+
+    //   devArr.push({"device":"Webcam","manufacturer":"Skalith","purchaseDate":"23.06.2011","price":39})
+    //   devArr.push({"device":"Monitor","manufacturer":"Roodel","purchaseDate":"26.04.2011","price":32})
+    //   devArr.push({"device":"Network card","manufacturer":"Twitterbeat","purchaseDate":"17.05.2014","price":10})
+    //   devArr.push({"device":"Digital camera","manufacturer":"Trilith","purchaseDate":"29.12.2013","price":21})
+    //   devArr.push({"device":"Sound Card","manufacturer":"Tagchat","purchaseDate":"14.07.2012","price":75})
+
+    //   collection.insertMany(devArr, (err, result) => {
+    //     if (err) {
+    //       console.log(err)
+    //     } else {
+    //       console.log('Inserted %d documents with "_id" %s', result.length, result)
+    //     }
+        db.close()
+    //   })
+    }
+  }
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Process function abort, because it manual PID killing is boring
 function exitProcess() {
     process.abort()
-
 }
 
 setTimeout(exitProcess, 3000);
