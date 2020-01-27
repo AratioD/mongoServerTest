cb33165aa111be0561571b6c45e28bc5920eadc6
AratioD <fffb@ddd.com>
2020-01-28 00:03:55 +0200

    connected to other database

--- mongo.js
+++ mongo.js
@@ -38,60 +38,30 @@
 //const mongodb = require('mongodb')
 const mongoClient = require('mongodb').MongoClient
 
-const url = 'mongodb://localhost:27017/customer'
+const url = 'mongodb://localhost:27017/users'
 
 // create new devices collection and add some data into it
 mongoClient.connect(url, {
     useNewUrlParser: true,
     useUnifiedTopology: true
-  },
-  (err, db) => {
-    if (err) {
-      console.log('ERROR:', err)
-    } 
-    else {
-      console.log('CONNECTED: ', url)
+},
+    (err, db) => {
+        if (err) {
+            console.log('ERROR:', err)
+        }
+        else {
+            console.log('CONNECTED: ', url)
+           // var db = mongoClient.db('customer');
 
-    //   const demoDb = db.db('nodedemos')
-
-    //   const collection = demoDb.collection('devices')
-
-    //   const devArr = []
-
-    //   devArr.push({"device":"Webcam","manufacturer":"Skalith","purchaseDate":"23.06.2011","price":39})
-    //   devArr.push({"device":"Monitor","manufacturer":"Roodel","purchaseDate":"26.04.2011","price":32})
-    //   devArr.push({"device":"Network card","manufacturer":"Twitterbeat","purchaseDate":"17.05.2014","price":10})
-    //   devArr.push({"device":"Digital camera","manufacturer":"Trilith","purchaseDate":"29.12.2013","price":21})
-    //   devArr.push({"device":"Sound Card","manufacturer":"Tagchat","purchaseDate":"14.07.2012","price":75})
-
-    //   collection.insertMany(devArr, (err, result) => {
-    //     if (err) {
-    //       console.log(err)
-    //     } else {
-    //       console.log('Inserted %d documents with "_id" %s', result.length, result)
-    //     }
-        db.close()
-    //   })
+  
+            // db.collection('customers').findOne({}, function (findErr, result) {
+            //     if (findErr) throw findErr;
+            //     console.log(result.name);
+            //     client.close();
+            // });
+        }
     }
-  }
 )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 //Process function abort, because it manual PID killing is boring
