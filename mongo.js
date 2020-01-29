d0c3218e78c9cd87054b896a786f51556e92826c
AratioD <fffb@ddd.com>
2020-01-29 20:04:20 +0200

    housekept the code

--- mongo.js
+++ mongo.js
@@ -1,23 +1,25 @@
 
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/backEnd";
+let MongoClient = require("mongodb").MongoClient
+let url = "mongodb://localhost:27017/"
+
+// my database name
+const dbName = "backEnd"
 
 MongoClient.connect(url, { useUnifiedTopology: true }, function (err, db) {
-    if (err) throw err;
-    var dbo = db.db("backEnd");
+    if (err) throw err
+    let dbo = db.db(dbName)
 
-    var myobj = { name: "Juu Company Inc", address: "Apple street 10" };
-    var myobj1 = { name: "SX SW development Inc", address: "Pizza street" };
+    let myobj = { name: "Juu Company Inc", address: "Apple street 10" }
+    let myobj1 = { name: "SX SW development Inc", address: "Pizza street" }
 
     dbo.collection("firstName").insertMany([myobj1, myobj], function (err, res) {
-        if (err) throw err;
-        console.log("1 document inserted");
-        db.close();
-    });
-
+        if (err) throw err
+        console.log("1 document inserted")
+        db.close()
+    })
     dbo.collection("backEnd").find({}).toArray(function (err, result) {
-        if (err) throw err;
-        console.log(result);
-        db.close();
-    });
-});
+        if (err) throw err
+        console.log(result)
+        db.close()
+    })
+})
